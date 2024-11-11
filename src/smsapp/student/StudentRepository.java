d2325c6e087c196573e71ea2f68426910ecfaaac
ypanator <153828092+ypanator@users.noreply.github.com>
2024-11-11 12:07:42 +0000

    Refactor the StudentRepository.java

--- src/smsapp/student/StudentRepository.java
+++ src/smsapp/student/StudentRepository.java
@@ -7,62 +7,35 @@ public class StudentRepository {
     private final String url = "jdbc:sqlite:sms_app.db";
 
     public StudentRepository() throws SQLException {
-        try (
-            Connection connection = DriverManager.getConnection(url);
-            Statement statement = connection.createStatement()
-        ) {
-            statement.executeUpdate(
-                """
-                CREATE TABLE IF NOT EXISTS students (
-                    studentID TEXT PRIMARY KEY,
-                    name TEXT NOT NULL,
-                    age INTEGER NOT NULL,
-                    grade REAL NOT NULL
-                );
-                """
+        runUpdateQuery(
+            """
+            CREATE TABLE IF NOT EXISTS students (
+                studentID TEXT PRIMARY KEY,
+                name TEXT NOT NULL,
+                age INTEGER NOT NULL,
+                grade REAL NOT NULL
             );
-
-        }
+            """
+        );
     }
     
     public void save(Student student) throws SQLException {
         String sql = "INSERT INTO students (studentID, name, age, grade) VALUES (?, ?, ?, ?);";
-
-        try (
-            Connection connection = DriverManager.getConnection(url);
-            PreparedStatement statement = connection.prepareStatement(sql)
-        ) {
-            statement.setString(1, student.getStudentID());
-            statement.setString(2, student.getName());
-            statement.setInt(3, student.getAge());
-            statement.setDouble(4, student.getGrade());
-
-            statement.executeUpdate();
-        }
+        runUpdateQuery(sql, 
+            student.getStudentID(), student.getName(), student.getAge(), student.getGrade()
+        );
     }
 
     public void update(Student student) throws SQLException {
         String sql = "UPDATE students SET name = ?, age = ?, grade = ? WHERE studentID = ?;";
-
-        try (
-            Connection connection = DriverManager.getConnection(url);
-            PreparedStatement statement = connection.prepareStatement(sql)
-        ) {
-            statement.setString(1, student.getName());
-            statement.setInt(2, student.getAge());
-            statement.setDouble(3, student.getGrade());
-            statement.setString(4, student.getStudentID());
-
-            statement.executeUpdate();
-        }
+        runUpdateQuery(sql, 
+            student.getName(), student.getAge(), student.getGrade(), student.getStudentID()
+        );
     }
     
     public ArrayList<Student> getAllStudents() throws SQLException {
-        try (
-            Connection connection = DriverManager.getConnection(url);
-            Statement statement = connection.createStatement();
-            ResultSet data = statement.executeQuery("SELECT * FROM students;")
-        ) {
+        String sql = "SELECT * FROM students;";
+        return runSelectQuery(sql, data -> {
             ArrayList<Student> students = new ArrayList<>();
 
             while (data.next()) {
@@ -75,50 +48,30 @@ public class StudentRepository {
             }
 
             return students;
-        }
+        });
     }
     
     public void remove(String studentID) throws SQLException {
         String sql = "DELETE FROM students WHERE studentID = ?;";
-        try (
-            Connection connection = DriverManager.getConnection(url);
-            PreparedStatement statement = connection.prepareStatement(sql)
-        ) {
-            statement.setString(1, studentID);
-            statement.executeUpdate();
-        }
+        runUpdateQuery(sql, studentID);
     }
 
     public boolean isPresent(String studentID) throws SQLException {
         String sql = "SELECT 1 FROM students WHERE studentID = ?;";
-        try (
-            Connection connection = DriverManager.getConnection(url);
-            PreparedStatement statement = connection.prepareStatement(sql);
-        ) {
-            statement.setString(1, studentID);
-            try (ResultSet data = statement.executeQuery()) {
-                return data.next();
-            }
-        }
+        return runSelectQuery(sql, data -> data.next(), studentID);
     }
 
     public Student getStudent(String studentID) throws SQLException {
         String sql = "SELECT * FROM students WHERE studentID = ?;";
-        try (
-            Connection connection = DriverManager.getConnection(url);
-            PreparedStatement statement = connection.prepareStatement(sql);
-        ) {
-            statement.setString(1, studentID);
-            try (ResultSet data = statement.executeQuery()) {
-                data.next();
-                return new Student(
-                    data.getString("name"), 
-                    data.getInt("age"), 
-                    data.getDouble("grade"),
-                    data.getString("studentID")
-                );
-            }
-        }
+        return runSelectQuery(sql, data -> {
+            data.next();
+            return new Student(
+                data.getString("name"), 
+                data.getInt("age"), 
+                data.getDouble("grade"),
+                data.getString("studentID")
+            );
+        }, studentID);
     }
 
     public String getName(String studentID) throws SQLException {
@@ -132,4 +85,49 @@ public class StudentRepository {
     public double getGrade(String studentID) throws SQLException {
         return getStudent(studentID).getGrade();
     }
+
+    private void runUpdateQuery(String sql, Object... params) throws SQLException {
+        try (Connection connection = DriverManager.getConnection(url)) {
+
+            if (params.length > 0) {
+                try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                    for (int i = 0; i < params.length; i++) {
+                        statement.setObject(i + 1, params[i]);
+                    }
+                    statement.executeUpdate();
+                }
+            } else {
+                try (Statement statement = connection.createStatement()) {
+                    statement.execute(sql);
+                }
+            }
+        }
+    }
+
+    private <R> R runSelectQuery(String sql, SQLFunction<R, ResultSet> function, Object... params) throws SQLException {
+        try (Connection connection = DriverManager.getConnection(url)) {
+
+            if (params.length > 0) {
+                try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                    for (int i = 0; i < params.length; i++) {
+                        statement.setObject(i + 1, params[i]);
+                    }
+                    try (ResultSet data = statement.executeQuery()) {
+                        return function.apply(data);
+                    }
+                }
+            } else {
+                try (Statement statement = connection.createStatement()) {
+                    try (ResultSet data = statement.executeQuery(sql)) {
+                        return function.apply(data);
+                    }
+                }
+            }
+        }
+    }
+}
+
+@FunctionalInterface
+interface SQLFunction<R, T> {
+    R apply(T t) throws SQLException;
 }
